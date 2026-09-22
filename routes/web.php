84ec4b9a8ce5e034827da4738ad829e74e17366f
0xdevvyy <pullhead45@gmail.com>
2026-09-22 21:47:41 +0800

    feat: added the landing page

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::inertia('/', 'Welcome')->name('home');
+// Route::inertia('/', 'Welcome')->name('home');
 
 Route::inertia('/test', 'Testing')->name('test');
+
+
+Route::middleware('guest')->group(function (): void {
+    Route::inertia('/', 'landing/Home')->name('home');
+});
